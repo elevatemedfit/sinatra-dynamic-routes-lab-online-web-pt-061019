0562038d0b9735abada5e97eba58497764e29463
Chris Junker <elevatemedfit@gmail.com>
2019-09-16 14:31:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
   @word4 = params[:word4]
   @word5 = params[:word5]
   @result = ("#{@word1} + #{@word2} + #{@word3} + #{@word4} + #{@word5}")
-
+@result.join(.)
 end
 
 end

2a1e6e72e46a88e63efdc0bdbb6b13f076b06b46
Chris Junker <elevatemedfit@gmail.com>
2019-09-16 14:23:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,17 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
   @number = params[:number].to_i
   @phrase = params[:phrase]
-  "#{@phrase}" * @number 
+  "#{@phrase}" * @number
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+  @word1 = params[:word1]
+  @word2 = params[:word2]
+  @word3 = params[:word3]
+  @word4 = params[:word4]
+  @word5 = params[:word5]
+  @file = word1 + word2 + word3 + word4 + word5
+  @file.join
 end
+
 end

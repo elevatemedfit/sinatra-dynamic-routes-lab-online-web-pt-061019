4d46d746a9df8dfec60cbaae40786817508c0471
Chris Junker <elevatemedfit@gmail.com>
2019-09-16 14:01:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
   @times_repeat = params[:number].to_i
-  @phrase = params[:phrase]
-  @times_repeat * "#{@phrase}".to_s
+  @phrase = params[:phrase].to_s
+
 end
 end

b3979a622403c30db56dbf822c76ad18e0bf47bb
Chris Junker <elevatemedfit@gmail.com>
2019-09-16 15:01:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,9 @@ get '/:operation/:number1/:number2' do
   when "subtract"
     (@number1 - @number2).to_s
   when "multiply"
-(@number1 * @number2).to_s
+    (@number1 * @number2).to_s
+  when "divide"
+    (@number1 / @number2).to_s
 end
 end
 end
